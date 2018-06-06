21cbb85a30a7e289544e63b390d9ee213414e369
heitorrapela <hrm@cin.ufpe.br>
2018-06-05 21:02:57 -0300

    Add package to screnshoot from image view (tiago camera)

---
+++ saveImage/saveImage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# rospy for the subscriber
+import rospy
+# ROS Image message
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+import cv2
+
+# Instantiate CvBridge
+bridge = CvBridge()
+j=0
+def image_callback(msg):
+    print("Received an image!")
+    global j
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg 
+        cv2.imwrite('{}.jpeg'.format(j), cv2_img)
+        j+=1
+
+def main():
+    rospy.init_node('image_listener')
+    # Define your image topic
+    image_topic = "/xtion/rgb/image_raw"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, image_callback)
+    # Spin until ctrl + c
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
